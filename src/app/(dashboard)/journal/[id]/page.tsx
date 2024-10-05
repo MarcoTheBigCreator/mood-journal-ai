68fd0670ef34b9947e9a81e63ae0012e38c0a86c
Marco Rodriguez <marcotbcreator@gmail.com>
2024-10-05 02:52:33 -0600

    Refactor journal page to add analysis section

--- src/app/(dashboard)/journal/[id]/page.tsx
+++ src/app/(dashboard)/journal/[id]/page.tsx
@@ -22,9 +22,53 @@ export default async function EntryPage({ params }: EntryPageProps) {
 
   const entry = await getEntry(journalUserId, id);
 
+  const analysisData = [
+    {
+      name: 'Summary',
+      value: '',
+    },
+    {
+      name: 'Subject',
+      value: '',
+    },
+    {
+      name: 'Mood',
+      value: '',
+    },
+    {
+      name: 'Negative',
+      value: '',
+    },
+    {
+      name: 'Recommendation',
+      value: '',
+    },
+  ];
+
   return (
-    <div className="h-full w-full">
-      <Editor entry={entry} />
+    <div className="h-full w-full grid grid-cols-3 space-x-3">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+
+      {/* TODO: Make this a component */}
+      <div className="border rounded-xl border-violet-400">
+        <div className="bg-blue-400 rounded-xl px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <div>
+          <ul>
+            {analysisData.map((data) => (
+              <li
+                key={data.name}
+                className="text-lg flex items-center justify-between"
+              >
+                <span className="font-bold">{data.name}:</span> {data.value}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
